3a9b971e5f4988cd643c0fe894b82e199f2807b6
Alejandro Torres <alejandrojuegoselx@gmail.com>
2020-10-14 11:50:32 +0200

    [ADD] Created detailsfragment with custom style

---
+++ app/src/main/java/com/example/pokedex_mvvm/ui/fragments/DetailFragment.kt
@@ -0,0 +1,21 @@
+package com.example.pokedex_mvvm.ui.fragments
+
+import android.content.Context
+import android.os.Bundle
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.pokedex_mvvm.R
+
+class DetailFragment : Fragment(R.layout.details_fragment) {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
